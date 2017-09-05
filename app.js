e4460a3adc153adce073a8490defd0b7d5bcad95
Mark Jackson <mark.j.jackson@me.com>
2017-09-05 20:31:25 +0200

    created optional CORS middleware for Express (for development, but might be useful in production)

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ const { constructDbUri } = require('./util/db')
 
 // Import environment variables
 require('dotenv').config()
+const { env } = process
 
 // Create express app
 const app = express()
+
+// Middleware
+
+// Configure CORS is desired (included in .env)
+if (env.APP_CORS_ORIGIN && env.APP_CORS_HEADERS) {
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', env.APP_CORS_ORIGIN)
+    res.header("Access-Control-Allow-Headers", env.APP_CORS_HEADERS)
+  })
+}
 
 // Define routes
 app.get('/', (req, res) => {
@@ -15,6 +26,6 @@ app.get('/', (req, res) => {
 })
 
 // Run app
-app.listen(process.env.APP_PORT, () => {
-  console.log(`App listening on port ${process.env.APP_PORT}`)
+app.listen(env.APP_PORT, () => {
+  console.log(`App listening on port ${env.APP_PORT}`)
 })
